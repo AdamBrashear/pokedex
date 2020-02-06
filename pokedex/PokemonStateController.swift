a8d342e2b9a19a4d65ed100b114acf575486ece4
FOGDevDamani <Damani@maryswatch.com>
2020-02-05 16:43:43 -0800

    added comment to provide clarity

--- pokedex/PokemonStateController.swift
+++ pokedex/PokemonStateController.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 
+//This controller keeps the state of the app.
+
 class PokemonStateController {
 	var pokemon = Pokemon(name: "Ditto", id: 132, weight: 40, height: 3, type: "normal", defense: 48, attack: 48, description: "Ditto rearranges its cell structure to transform itself into other shapes. However, if it tries to transform itself into something by relying on its memory, this Pokémon manages to get details wrong.", baseExperience: 101)
 }
