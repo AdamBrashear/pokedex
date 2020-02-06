68989a21b803916284ceef6e4115e453bee204f6
FOGDevDamani <Damani@maryswatch.com>
2020-02-05 16:41:26 -0800

    Added pokemon images Added model and statecontroller

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,21 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Damani Turner on 2/5/20.
+//  Copyright © 2020 TurnerDevelopment. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+	let name: String
+	let id: Int
+	let weight: Int
+	let height: Int
+	let type: String
+	let defense: Int
+	let attack: Int
+	let description: String
+	let baseExperience: Int
+}
---
+++ pokedex/PokemonStateController.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonStateController.swift
+//  pokedex
+//
+//  Created by Damani Turner on 2/5/20.
+//  Copyright © 2020 TurnerDevelopment. All rights reserved.
+//
+
+import Foundation
+
+
+class PokemonStateController {
+	var pokemon = Pokemon(name: "Ditto", id: 132, weight: 40, height: 3, type: "normal", defense: 48, attack: 48, description: "Ditto rearranges its cell structure to transform itself into other shapes. However, if it tries to transform itself into something by relying on its memory, this Pokémon manages to get details wrong.", baseExperience: 101)
+}
